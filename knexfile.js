55c5778564a0ca2bb8d93e24ab4060b41e154ef0
Christopher Espiritu <christopheredrian@gmail.com>
2020-05-26 00:31:50 -0400

    pg support

--- knexfile.js
+++ knexfile.js
@@ -40,9 +40,10 @@ module.exports = {
   production: {
     client: process.env.DB_CLIENT || 'mysql2',
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
